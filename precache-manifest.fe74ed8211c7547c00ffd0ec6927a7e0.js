616950925f530d244653843efa470a7d5ab56ad9
zhanghaohua <haohua@bom2buy.com>
2020-07-01 11:25:09 +0800

    Updates

---
+++ precache-manifest.fe74ed8211c7547c00ffd0ec6927a7e0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de402a2bd9a91794fbb1327d9999de5e",
+    "url": "/test/index.html"
+  },
+  {
+    "revision": "5ba8456fb511508cd5b0",
+    "url": "/test/static/css/main.f6eebb68.chunk.css"
+  },
+  {
+    "revision": "d2eb9c46fcaa83b0e73d",
+    "url": "/test/static/js/2.29cd6083.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/test/static/js/2.29cd6083.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5ba8456fb511508cd5b0",
+    "url": "/test/static/js/main.e1619c6b.chunk.js"
+  },
+  {
+    "revision": "29d9a3cf2ee130fa9d36",
+    "url": "/test/static/js/runtime-main.2c1f07ac.js"
+  }
+]);
